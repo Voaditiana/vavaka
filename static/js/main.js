00de7de038b7ec08e0d74151bc97121a90869910
Voaditiana <voaditiana@gmail.com>
2026-01-21 16:03:16 +0300

    Update regex for reference extraction to support Unicode

--- static/js/main.js
+++ static/js/main.js
@@ -3,7 +3,7 @@ try{
   const txt_date=document.getElementById("txt-date");
   const lines= versets.split("\n");
   	const getDate=l=>/\d{4}-\d{2}-\d{2}/.exec(l)[0];
-      	const getRef=l=>/\d*\s?\w+ \d+,\s?\d+/.exec(l)[0];
+      	const getRef=l=>/\d*\s?\p{L}+ \d+,\s?\d+/u.exec(l)[0];
       	const getCont=l=>(/,"[^"]+"/.exec(l)[0]).substring(1);
       	console.log(getCont(lines[3]));
    const malagasyLocale = {
